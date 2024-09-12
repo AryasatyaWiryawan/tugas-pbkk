0cc637d2fabb116b352ff857aad8d83b62f169e7
unknown <aryasatyapark@gmail.com>
2024-09-12 16:40:10 +0700

    Setelah melihat video WPU UNPAS

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3>This is About section</h3>
+    <p>Nama: {{ $name }}</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Post::find($slug);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Awan', 'title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
